f515599d6b944a6e26ea12865e57863bf3cc40dd
abhishekkarky <karkyabhishek@gmail.com>
2023-03-26 10:59:12 +0545

    Spring Initialized

---
+++ src/main/java/com/system/hostel_booking_system/HostelBookingApplication.java
@@ -0,0 +1,13 @@
+package com.system.hostel_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostelBookingApplication.class, args);
+    }
+
+}
